ed5d5ab7c24402bc966e1a19af384e63537a4b56
amihan <amirkhan.shamgonov.0101@mail.ru>
2023-10-26 00:17:18 +0600

    добавление интернационализации

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,22 +2,23 @@
 import './styles/index.scss'
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/className';
-
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { AppRouter } from './providers/router';
+import { Suspense } from 'react';
 
 const App = () => {
     const { theme } = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
-
+            <Suspense fallback=''>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div >
     );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,15 @@
+import { useTranslation } from "react-i18next";
+
+
+
+
 const MainPage = () => {
+
+    const { t } = useTranslation()
+
     return (
         <div>
-            MainPage
+            {t('Главная страница')}
         </div>
     );
 };
