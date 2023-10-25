c254433426a5edc8536c550aec564826bb0900c7
amihan <amirkhan.shamgonov.0101@mail.ru>
2023-10-25 06:13:42 +0600

    add svg loader and file loader

---
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,31 @@
+import { classNames } from 'shared/lib/className';
+import cls from './ThemeSwitcher.module.scss'
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import { Button } from 'shared/ui/Button';
+import { ThemeButton } from 'shared/ui/Button/ui/Button';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+
+
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+
+    const { theme, toggleTheme } = useTheme()
+
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            onClick={toggleTheme}
+        >
+
+            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+        </Button>
+    );
+};
+
+export default ThemeSwitcher;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/className';
 import cls from './Navbar.module.scss'
-import AppLink, { AppLinkTheme } from 'shared/ui/AppLink';
+import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 
 interface NavbarProps {
@@ -11,6 +12,7 @@ interface NavbarProps {
 const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink
                     to={'/'}
